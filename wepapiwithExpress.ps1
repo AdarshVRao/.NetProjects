f4ef415150af4fa37571784ba2892b94474c9860
Adarsh Rao <82211020+AdarshVRao@users.noreply.github.com>
2021-06-06 12:52:45 +0530

    Add files via upload

---
+++ wepapiwithExpress.ps1
@@ -0,0 +1,35 @@
+﻿$Respons = Invoke-RestMethod -Uri 'http://localhost:3000/ticket' -Method Get 
+
+$Respons = Invoke-RestMethod -Uri 'http://localhost:3000/ticket/200' -Method Get 
+
+$Respons = Invoke-RestMethod -Uri 'http://localhost:3000/ticket' -Method Post 
+
+$Respons = Invoke-RestMethod -Uri 'http://localhost:3000/ticket' -Method Put 
+
+$Respons = Invoke-RestMethod -Uri 'http://localhost:3000/ticket/101' -Method Delete 
+
+
+
+$Respons = Invoke-RestMethod -Uri 'https://localhost:44303/api/ticket/' -Method Get 
+$Respons
+
+$Respons = Invoke-RestMethod -Uri 'https://localhost:44303/api/ticket/200' -Method Get 
+$Respons
+
+$Respons = Invoke-RestMethod -Uri 'https://localhost:44303/api/ticket' -Method Post 
+$Respons
+
+$Respons = Invoke-RestMethod -Uri 'https://localhost:44303/api/ticket' -Method Put 
+
+$Respons
+
+$Respons = Invoke-RestMethod -Uri 'https://localhost:44303/api/ticket/201' -Method Delete 
+$Respons
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+$Respons = Invoke-RestMethod -Uri 'https://localhost:44303/api/project/56/tickets' -Method Get 
+$Respons
+
+$body ={ }
+
+$Respons = Invoke-RestMethod -Uri 'https://localhost:44303/api/ticket' -Method Post 
+$Respons
